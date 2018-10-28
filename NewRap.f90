746795fbc09fce1e0df8f8ca28ed10f6290c21c2
clivan <claudio.ivan01@gmail.com>
2018-10-27 20:53:46 -0500

    Add files via upload

--- NewRap.f90
+++ NewRap.f90
@@ -1,23 +1,26 @@
 !Autor: Claudio Iván Esparza Castañeda
-!Título: Newton-Raphson simple
-!Descripción: Programa que calcula las raíces de una función por medio del método de Newton-Raphson
-!Fecha: 21/10/2018
+!Título: Newton-Raphson para sistema de ecuaciones
+!Descripción: Programa que calcula las raíces de una sistema de ecuaciones no lineales
+!Fecha: 27/10/2018
   
 
 Program newrap
   implicit none
-  REAL::e, er, x, y, f, fp, xe
+  REAL::e, er, x, y, xe, u, v, pux, puy, pvx, pvy, J
   INTEGER::i
-  CALL graf1()
-  WRITE(*, *) "Valor inicial" !Preguntar final del intervalo
+  WRITE(*, *) "Valor inicial de x" !Preguntar final del intervalo
   READ(*, *) x !Ingresar final del intervalo
+  WRITE(*, *) "Valor inicial de y" !Preguntar final del intervalo
+  READ(*, *) y !Ingresar final del intervalo
   WRITE(*, *) "Error esperado" !Preguntar el error
   READ(*, *) er !Ingresar el error
   i=0 !Inicializar contador de ciclos
   do
      i=i+1 !Añadir uno al contador
      xe=x !Asignar x anterior
-     x=x-fp(x)/f(x)
+     J=pux(x, y)*pvy(x, y)-puy(x, y)*pvx(x, y)
+     x=x-(u(x, y)*pvy(x, y)-v(x, y)*puy(x, y))/J
+     y=y-(v(x, y)*pux(x, y)-u(x, y)*pvx(x, y))/J
      if (x .NE. 0) then !Validación del intervalo
         e=abs((x-xe)/x)*100 !Cálculo del error
      end if
@@ -25,89 +28,47 @@ Program newrap
         EXIT
      end if
   end do
-  write(*, *) "       x                 f(x)            e               i" !Cabecera del resultado
-  write(*, *) x, f(x), e, i !Imprimir resultado
-  open(unit=3, file="Punto.dat", status="unknown") !Archivo para graficar el punto de intersección
-  write(3, *) x, f(x) !Escribir en archivo
+  write(*, *) "          i        x                y               e" !Cabecera del resultado
+  write(*, *) i, x, y, e!Imprimir resultado
   close(3)
-  CALL graf2()
 end Program newrap
 
-subroutine graf1() !subrutina que grafica
-  implicit none !Quitar variables implícitas
-  REAL::x, y, f, h, a, b !Variables de tipo real
-  INTEGER::N, i !Variables de tipo entero
-  open(unit=1, file="Datos.dat", status="unknown") !Abrir archivo para guardar puntos
-  open(unit=2, file="Graf.plt", status="unknown") !Abrir archivo para guardar información de la gŕafica
-  WRITE(*, *) "Inicio de la gráfica" !Preguntar inicio de gráfica
-  READ(*, *) a !Ingresar inicio de la gráfica
-  WRITE(*, *) "Final de la gráfica" !Preguntar final de la gráfica
-  READ(*, *) b !Ingresar final de la gráfica
-  WRITE(*, *) "¿Con cuántos puntos?" !Preguntar número de puntos
-  READ(*, *) N !Ingresar número de puntos
-  h=(b-a)/N !Tamaño de paso
-  do i=0, N !Construir datos
-     x=a+i*h 
-     y=f(x)
-     WRITE(1, *) x, y !Escribir datos en archivo
-  end do
-  close(1)
-  !Establecer todos los parámetros de la gráfica
-  WRITE(2, *) "set title 'y=sin(x)-cos(x)'"
-  WRITE(2, *) "set xlabel 'x'"
-  WRITE(2, *) "set ylabel 'y'"
-  WRITE(2, *) "m='./Datos.dat'"
-  WRITE(2, *) "set nokey"
-  WRITE(2, *) "set grid"
-  WRITE(2, *) "p m w l"
-  WRITE(2, *) "replot"
-  close(2)
-  CALL SYSTEM("gnuplot -p Graf.plt") !Llamar a GNUPlot desde la terminal
-end subroutine graf1
-
-subroutine graf2() !subrutina que grafica
-  implicit none !Quitar variables implícitas
-  REAL::x, y, f, h, a, b !Variables de tipo real
-  INTEGER::N, i !Variables de tipo entero
-  open(unit=1, file="Datos.dat", status="unknown") !Abrir archivo para guardar puntos
-  open(unit=2, file="Graf.plt", status="unknown") !Abrir archivo para guardar información de la gŕafica
-  WRITE(*, *) "Inicio de la gráfica" !Preguntar inicio de gráfica
-  READ(*, *) a !Ingresar inicio de la gráfica
-  WRITE(*, *) "Final de la gráfica" !Preguntar final de la gráfica
-  READ(*, *) b !Ingresar final de la gráfica
-  WRITE(*, *) "¿Con cuántos puntos?" !Preguntar número de puntos
-  READ(*, *) N !Ingresar número de puntos
-  h=(b-a)/N !Tamaño de paso
-  do i=0, N !Construir datos
-     x=a+i*h 
-     y=f(x)
-     WRITE(1, *) x, y !Escribir datos en archivo
-  end do
-  close(1)
-  !Establecer todos los parámetros de la gráfica
-  WRITE(2, *) "set title 'y=sin(x)-cos(x)'"
-  WRITE(2, *) "set xlabel 'x'"
-  WRITE(2, *) "set ylabel 'y'"
-  WRITE(2, *) "m='./Datos.dat'"
-  WRITE(2, *) "n='./Punto.dat'"
-  WRITE(2, *) "set nokey"
-  WRITE(2, *) "set grid"
-  WRITE(2, *) "p m w l, n"
-  WRITE(2, *) "set term png"
-  WRITE(2, *) "set out 'Graf.png'"
-  WRITE(2, *) "replot"
-  close(2)
-  CALL SYSTEM("gnuplot -p Graf.plt") !Llamar a GNUPlot desde la terminal
-end subroutine graf2
-
-function f(x) !Función que genera los puntos
+function u(x, y) !Primera ecuación
   implicit none
-  REAL::f, x
-  f=sin(x)-cos(x)
-end function f
+  REAL::x, y, u
+  u=x**2+x*y-10
+end function u
 
-function fp(x) !Función derivada
+function pux(x, y) !Derivada parcial de u con respecto a x
   implicit none
-  REAL::fp, x
-  fp=cos(x)+sin(x)
-end function fp
+  REAL::x, y, pux
+  pux=2*x+y
+end function pux
+
+function puy(x, y) !Derivada parcial de u con respecto a y
+  implicit none
+  REAL::x, y, puy
+  puy=x
+end function puy
+
+function v(x, y) !Segunda ecuación
+  implicit none
+  REAL::x, y, v
+  v=y+3*x*y**2-57
+end function v
+
+function pvx(x, y) !Derivada parcial de v con respecto a x
+  implicit none
+  REAL::x, y, pvx
+  pvx=3*x**2
+end function pvx
+
+function pvy(x, y) !Derivada parcial de v con respecto a y
+  implicit none
+  REAL::x, y, pvy
+  pvy=1+6*x*y
+end function pvy
+
+
+
+
